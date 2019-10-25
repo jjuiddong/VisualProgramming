7295be7206432b6c77f094cd0bdabd7a3842e345
jjuiddong <jjuiddong@gmail.com>
2019-10-26 00:40:22 +0900

    update Node Editor, BluePrint Rendering

--- Src/vProgEditor/Editor/stdafx.h
+++ Src/vProgEditor/Editor/stdafx.h
@@ -5,11 +5,6 @@
 #include "../../../../Common/Network2/network2.h"
 
 
-//# include "../../../../Common/Framework11/external/nodeeditor/imgui_node_editor.h"
-//# define IMGUI_DEFINE_MATH_OPERATORS
-//# include "../../../../Common/Framework11/external/nodeeditor/imgui_internal.h"
-
-
 using common::StrId;
 using common::Vector3;
 
